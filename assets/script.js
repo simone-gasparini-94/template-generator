058514ab96c29399a43b0ab2ca1d3b2051e5452d
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-02-25 21:35:22 +0100

    Divided in different grids

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,33 @@
-const grid = document.querySelector(".grid");
-const cells = document.querySelectorAll(".cell");
+const createGrids = function(rows, columns) {
+    const container = document.querySelector(".container");
 
-const resetClasses = function() {
-    cells.forEach((cell) => {
-        cell.classList.remove("big-circle", "big-square", "small-circle", "small-square", "square-circle", "circle-square");
-    })
+    for (let i = 0; i < rows * columns; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+    }
+
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid => {
+        const randomizer = Math.floor(Math.random() * 2);
+        if(randomizer === 1) createCells(grid);
+    }))
 }
-const addShape = function() {  
+
+const createCells = function(grid) {
+    const rows = 8;
+    const columns = 4;
+    for (let i = 0; i < rows * columns; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        grid.appendChild(cell);
+    }
+
+    const cells = grid.querySelectorAll(".cell");
+    addShape(cells);
+}
+
+const addShape = function(cells) {  
     cells.forEach((cell => {
         const randomizer = Math.floor(Math.random() * 6);
 
@@ -19,29 +40,11 @@ const addShape = function() {
     })) 
 }
 
-const addBigCircle = function(cell) {
-    cell.classList.add("big-circle");
-}
+const addBigCircle = (cell) => cell.classList.add("big-circle");
+const addBigSquare = (cell) => cell.classList.add("big-square");
+const addSmallCircle = (cell) => cell.classList.add("small-circle");
+const addSmallSquare = (cell) => cell.classList.add("small-square");
+const addSquareCircle = (cell) => cell.classList.add("square-circle");
+const addCircleSquare = (cell) => cell.classList.add("circle-square");
 
-const addBigSquare = function(cell) {
-    cell.classList.add("big-square")
-}
-
-const addSmallCircle = function(cell) {
-    cell.classList.add("small-circle");
-}
-
-const addSmallSquare = function(cell) {
-    cell.classList.add("small-square");
-}
-
-const addSquareCircle = function(cell) {
-    cell.classList.add("square-circle");
-}
-
-const addCircleSquare = function(cell) {
-    cell.classList.add("circle-square");
-}
-
-document.addEventListener("DOMContentLoaded", addShape);
-
+document.addEventListener("DOMContentLoaded", () => createGrids(4, 3));
